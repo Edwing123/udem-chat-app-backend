0a837f046c27cf473e353fe02f089ab6da354d84
Edwing123 <egarciavalle2014@gmail.com>
2022-11-24 03:21:40 -0600

    chore: add and modify types at `types.go`
    The type for the fields `RPort` and `DbPort` now is `uint18`,
    which seems more logical since a port number is an unsigned
    16 bits integers.
    
    Add field `Addr` to struct `Flags`, this flag will be used to specify
    the address where the HTTP server will be listening.
    
    Add a new struct type `Global`, the purpose of this type is to
    work as a glue/namespacing type, for example, it has a field called
    `Logger`, this logger then can be used by all attached methods of this
    struct type (for example, used by middlewares and handlers.).
    
    BREAKING CHANGE: all places where the modified types are used should update/adapt.

--- cmd/api/types.go
+++ cmd/api/types.go
@@ -1,17 +1,27 @@
 package main
 
+import (
+	"github.com/gofiber/fiber/v2/middleware/session"
+	"golang.org/x/exp/slog"
+)
+
+type Global struct {
+	Logger *slog.Logger
+	Store  *session.Store
+}
+
 // Config represents the configuration file.
 type Config struct {
 	// Redis connection details.
 	RUser     string `json:"rUser"`
 	RPassword string `json:"rPassword"`
-	RPort     uint   `json:"rPort"`
+	RPort     uint16 `json:"rPort"`
 	RHost     string `json:"rHost"`
 
 	// Database connection details.
 	DbUser     string `json:"dbUser"`
 	DbPassword string `json:"dbPassword"`
-	DbPort     uint   `json:"dbPort"`
+	DbPort     uint16 `json:"dbPort"`
 	DbHost     string `json:"dbHost"`
 }
 
@@ -20,4 +30,5 @@ type Config struct {
 type Flags struct {
 	ConfigPath  string // Path of the configuration file.
 	LogsDirPath string // Path of the dir where the logs file will be stored.
+	Addr        string // Address for the server to listen on.
 }
