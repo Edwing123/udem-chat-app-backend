b7e44831baf376bdb851392444776afbc56095c7
Edwing123 <egarciavalle2014@gmail.com>
2022-11-30 23:23:45 -0600

    chore: add models for the table `User`
    A new type `User` is added, this represents values
    from the database table `User`.
    
    A new interface type `UserManager` is added, it defines
    methods that will be used to manage users from the database.

--- pkg/models/interfaces.go
+++ pkg/models/interfaces.go
@@ -1,1 +1,9 @@
 package models
+
+type UserManager interface {
+	New(user User) error
+	Get(id int) (User, error)
+	Login(user User) (int, error)
+	Update(user User) error
+	ChangePassword(id int, currentPass, newPass string) error
+}
--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,1 +1,9 @@
 package models
+
+type User struct {
+	Id               int    `json:"id,omitempty"`
+	Name             string `json:"name,omitempty"`
+	Password         string `json:"password,omitempty"`
+	Birthdate        string `json:"birthdate,omitempty"`
+	ProfilePictureId string `json:"profilePictureId,omitempty"`
+}
