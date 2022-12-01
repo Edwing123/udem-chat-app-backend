7e666dc481b0062a0251472277c6c228ea2b6226
Edwing123 <egarciavalle2014@gmail.com>
2022-12-01 01:33:17 -0600

    chore: update `UserManager.Update` to take user id.
    Now the method `Update` defined in the interface `UserManager`
    takes in as first argument the id of the user.

--- pkg/models/interfaces.go
+++ pkg/models/interfaces.go
@@ -4,6 +4,6 @@ type UserManager interface {
 	New(user User) error
 	Get(id int) (User, error)
 	Login(user User) (int, error)
-	Update(user User) error
+	Update(id int, user User) error
 	ChangePassword(id int, currentPass, newPass string) error
 }
