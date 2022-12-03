580eebba25feadb3a9b36fed9f175eadc465dfd7
Edwing123 <egarciavalle2014@gmail.com>
2022-12-02 21:44:48 -0600

    chore: add error codes and constants
    The previous commit mentioned "Use of new error codes", but those new
    error codes are nowhere to be seen, that's because I forgot to add to
    the commit the new files.
    
    Also, the previous commit is using constants, I forget about adding those, too
    
    This commit adds those missing files ':)

---
+++ pkg/models/constants.go
@@ -0,0 +1,7 @@
+package models
+
+const (
+	UserNameMaxLength          = 40
+	UserPasswordLength         = 60
+	UserProfilePictureIdLength = 36
+)
---
+++ pkg/models/errors.go
@@ -0,0 +1,24 @@
+package models
+
+import "github.com/Edwing123/udem-chat-app/pkg/codes"
+
+var (
+	// User errors.
+	ErrUserNameExists                     = codes.NewCode("user_name_exists")
+	ErrUserNameEmpty                      = codes.NewCode("user_name_empty")
+	ErrUserPasswordEmpty                  = codes.NewCode("user_password_empty")
+	ErrUserBirthdateEmpty                 = codes.NewCode("user_birthdate_empty")
+	ErrUserProfilePictureIdExists         = codes.NewCode("user_profile_picture_id_exists")
+	ErrUserNameExceedsMaxLength           = codes.NewCode("user_name_exceeds_max_length")
+	ErrUserPasswordNotValidLength         = codes.NewCode("user_password_not_valid_length")
+	ErrUserProfilePictureIdNotValidLength = codes.NewCode("user_profile_picture_id_not_valid_length")
+
+	// Authentication and password change errors.
+	ErrPasswordMismatch = codes.NewCode("password_mismatch")
+	ErrLoginFail        = codes.NewCode("login_fail")
+
+	// Generic database errors.
+	ErrNoRecords          = codes.NewCode("no_records")
+	ErrDatabaseServerFail = codes.NewCode("database_server_fail")
+	ErrNoUpdates          = codes.NewCode("no_updates_performed")
+)
