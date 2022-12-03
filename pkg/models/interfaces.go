c35eff57fb7e105dade2335b67884e5df0c59c85
Edwing123 <egarciavalle2014@gmail.com>
2022-12-02 20:27:33 -0600

    refactor!: change interface `UserManager.Update` signature
    Due to this change, implementations of this interface
    should be updated.

--- pkg/models/interfaces.go
+++ pkg/models/interfaces.go
@@ -4,6 +4,6 @@ type UserManager interface {
 	New(user User) error
 	Get(id int) (User, error)
 	Login(user User) (int, error)
-	Update(id int, user User) error
+	Update(id int, user User) (User, error)
 	ChangePassword(id int, currentPass, newPass string) error
 }
