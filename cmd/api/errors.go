93570a6fb01466476bbebb5ffa2b7cafde37b255
Edwing123 <egarciavalle2014@gmail.com>
2022-12-03 01:03:51 -0600

    chore: add more error codes

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -7,8 +7,9 @@ var (
 	ErrServerInternal = codes.NewCode("server_internal")
 
 	// Client related.
-	ErrCannotDecodeJSON = codes.NewCode(("cannot_decode_json"))
-	ErrAuthRequired     = codes.NewCode("auth_required")
+	ErrCannotDecodeJSON   = codes.NewCode(("cannot_decode_json"))
+	ErrAuthRequired       = codes.NewCode("auth_required")
+	ErrProfileImageTooBig = codes.NewCode("profile_image_too_big")
 
 	// Validation related.
 	ErrPasswordNotValid = codes.NewCode("password_not_valid")
