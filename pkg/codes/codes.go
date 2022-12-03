76a015a31f9d31554cd7a7b5991061f59e707ab5
Edwing123 <egarciavalle2014@gmail.com>
2022-12-02 21:48:57 -0600

    refactor!: remove unnecessary errors
    Now the `codes` package only exposes a type and a function for
    creating error codes.
    
    The packages themselves are the ones defining the error codes using the
    type and function provided from the `codes` package.

--- pkg/codes/codes.go
+++ pkg/codes/codes.go
@@ -13,24 +13,13 @@ func (c Code) Error() string {
 }
 
 var (
-	ErrUserNameAlreadyExists     = NewCode("user_name_already_exists")
-	ErrUserNameEmpty             = NewCode("user_name_empty")
-	ErrUserPasswordEmpty         = NewCode("user_password_empty")
-	ErrUserBirtdateEmpty         = NewCode("user_birthdate_empty")
-	ErrUserProfilePictureIdEmpty = NewCode("user_profile_picture_id_empty")
-	ErrPasswordsMismatch         = NewCode("current_password_new_password_mismatch")
+	// Server related.
+	ErrServerInternal = NewCode("server_internal")
 
-	ErrNoRecords           = NewCode("no_records")
-	ErrLoginFail           = NewCode("login_fail")
-	ErrDatabaseFail        = NewCode("database_fail")
-	ErrPasswordHashingFail = NewCode("password_hashing_fail")
-	ErrNoUpdatesToPerform  = NewCode("no_updates_to_perform")
+	// Client related.
+	ErrCannotDecodeJSON = NewCode(("cannot_decode_json"))
+	ErrAuthRequired     = NewCode("auth_required")
 
-	ErrNotLoggedIn           = NewCode("not_logged_in")
-	ErrServerInternal        = NewCode("server_internal")
-	ErrClient                = NewCode("client_error")
-	ErrPasswordNotValid      = NewCode("password_not_valid")
-	ErrAuthRequired          = NewCode("auth_required")
-	ErrProfileImageTooBig    = NewCode("profile_image_too_big")
-	ErrImageTypeNotSupported = NewCode("image_type_not_supported")
+	// Validation related.
+	ErrPasswordNotValid = NewCode("password_not_valid")
 )
